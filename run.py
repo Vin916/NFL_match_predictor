a6c0dfc234862cea142df9f0b28b03681403accd
Vin916 <vineethr767@gmail.com>
2025-09-27 23:02:52 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,3 @@
 # Run predictions for week 1:python3 nfl_weekly_predictor.py 1
 #Analyze data sources: python3 show_data_sources.py
 #Test auto-update: python3 test_auto_update.py
-#Run predictor: python3 nfl_predictor.py
