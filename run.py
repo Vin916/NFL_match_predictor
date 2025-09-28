d07ec0e3d671806e8fe21f0a5a02e46f025ecb50
Vin916 <vineethr767@gmail.com>
2025-09-27 22:59:47 -0400

    Create run.py

---
+++ run.py
@@ -0,0 +1,5 @@
+# Show upcoming weeks: python3 show_upcoming_weeks.py
+# Run predictions for week 1:python3 nfl_weekly_predictor.py 1
+#Analyze data sources: python3 show_data_sources.py
+#Test auto-update: python3 test_auto_update.py
+#Run predictor: python3 nfl_predictor.py
